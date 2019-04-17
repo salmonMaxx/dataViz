75ebc26c5b39639234c1f7c4e74f6b5472560afb
salmonMaxx <maxxwallberg@gmail.com>
2019-04-17 12:19:26 +0200

    started working on routing and cleaned the code a bit

--- lib/main.dart
+++ lib/main.dart
@@ -9,35 +9,28 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'flutter home',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+
+      // ===============================
+      // ========== ROUTES =============
+      // ===============================
+
+      initialRoute: '/',
+      routes: {
+        '/': (context) => MyHomePage(title: 'flutter home'),
+        'first': (context) => FirstRoute(),
+        'second': (context) => SecondRoute(),
+      },
+      home: FirstRoute(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -48,7 +41,6 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
 
-
   void doTheThing(){
     permissionContact();
     _incrementCounter();
@@ -56,11 +48,6 @@ class _MyHomePageState extends State<MyHomePage> {
 
   void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
       _counter++;
     });
   }
@@ -80,11 +67,11 @@ class _MyHomePageState extends State<MyHomePage> {
     PermissionGroup cameraPermission         = PermissionGroup.camera;
     PermissionGroup microphonePermission     = PermissionGroup.microphone;
 
-    //    A request followed by a rationale will
-    permissionsRequestCalendar   = requestPermissionFor([cameraPermission, calendarPermission]);                            //    give the user a dialogue
+                                                                                                      //    A request followed by a rationale will
+    permissionsRequestCalendar   = requestPermissionFor([cameraPermission, calendarPermission]);      //    give the user a dialogue (alert-box)
     bool cameraShow          = await PermissionHandler()                                              //    with the allow/deny choice
         .shouldShowRequestPermissionRationale(cameraPermission);
-    bool calendarShow          = await PermissionHandler()                                              //    with the allow/deny choice
+    bool calendarShow          = await PermissionHandler()
         .shouldShowRequestPermissionRationale(calendarPermission);
     //REQUEST STATUS
     /*
@@ -143,40 +130,21 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center( //LÄGG KNAPP FÖR PERMISSION HÄR BOI
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      body: Center(
         child: Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
               'You have pushed the button this many times:',
+            ),
+            RaisedButton(
+              onPressed: _incrementCounter,
+              child:
+                Text("increment")
             ),
             Text(
               '$_counter',
@@ -189,7 +157,70 @@ class _MyHomePageState extends State<MyHomePage> {
         onPressed: doTheThing,
         tooltip: 'Does both the permissions and increment',
         child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
+    );
+
+  }
+}
+
+class FirstRoute extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('back'),
+      ),
+      body: Center(
+        child: RaisedButton(
+          child: Text('Go away'),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => SecondRoute())
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
+
+class SecondRoute extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Away"),
+      ),
+      body: Center(
+        child: Container(
+          height: 100.0,
+          child: RaisedButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+            child: Row( mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Icon(Icons.attach_money),
+                Icon(Icons.attach_money),
+                Icon(Icons.attach_money),
+                Text(
+                    'Go back!'
+                ),
+                Icon(Icons.attach_money),
+                Icon(Icons.attach_money),
+                Icon(Icons.attach_money),
+                IconButton(
+                  icon: Icon(Icons.attach_money),
+                  onPressed: () {
+                    print ('dollar dollar bill y`all');
+                  }
+                ),
+              ]),
+              color: Colors.blue,
+          ),
+        ),
+      ),
     );
   }
 }
