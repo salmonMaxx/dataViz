2c26844788ecb310ff0499013d890a8a5ca1b8b5
CJCCarlberg <cj@castencarlberg,se>
2019-05-23 22:54:59 +0200

    Minor text changes

--- lib/routes/permissions/Phone.dart
+++ lib/routes/permissions/Phone.dart
@@ -19,7 +19,7 @@ class _PhoneState extends State<Phone> {
   // BOX 1
   String myImage = "assets/phone2.jpg";
   String myHeader = "Who would you like us to call?";
-  String myText = "The phone permission is regarded as one of the most vulnerable permissions since it allows for an app to make calls from your phone without asking the user. Even though it is fortuneatly very uncommon this could allow the app to call numbers which requieres you to pay money. Furthermore, an app with this permission could open your dialer with an already written number just for the user to confirm.";
+  String myText = "\nThe phone permission is regarded as one of the most vulnerable permissions since it allows for an app to make calls from your phone without asking the user. Even though it is fortuneatly very uncommon this could allow the app to call numbers which requieres you to pay money. Furthermore, an app with this permission could open your dialer with an already written number just for the user to confirm.";
 
   @override
   Widget build(BuildContext context) {
@@ -34,7 +34,7 @@ class _PhoneState extends State<Phone> {
         children: <Widget>[
           new Container(
             //Put in functions from the template below!!!
-            child: template.textBoxWithPic(null, myImage, myHeader , 18, myText, 15, 100),
+            child: template.textBoxWithPic(null, myImage, myHeader , 22, myText, 15, 100),
           ),
           new Container(
           child: template.otherPermissionBox(null, null, widget.whoHasPhone),
